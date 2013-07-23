b3b42c651b1902171c9012e0e596a96b9f3542ed
Ryan Wilson <syndicated.life@gmail.com>
2013-07-22 20:32:18 -0700

    updated resources and added unit test basics

---
+++ ffxivapp-v2/FFXIVAPP.Plugin.Event.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿// FFXIVAPP.Plugin.Event.Tests
+// UnitTest1.cs
+//  
+// Created by Ryan Wilson.
+// Copyright © 2007-2013 Ryan Wilson - All Rights Reserved
+
+#region Usings
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#endregion
+
+namespace FFXIVAPP.Plugin.Event.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
