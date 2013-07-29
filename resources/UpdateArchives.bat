05789cfc703239d102c78106dbb5f3d6e28d3132
Ryan Wilson <syndicated.life@gmail.com>
2013-07-29 14:21:41 -0700

    added resources

---
+++ resources/UpdateArchives.bat
@@ -0,0 +1,77 @@
+cd "..\build\Debug"
+del Debug.zip /q
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip FFXIVAPP.Client.exe
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip FFXIVAPP.Client.exe.config
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip FFXIVAPP.Client.exe.nlog
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip FFXIVAPP.Common.dll
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip FFXIVAPP.IPluginInterface.dll
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip FFXIVAPP.Updater.exe
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip HtmlAgilityPack.dll
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip Ionic.Zip.dll
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip MahApps.Metro.dll
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip Newtonsoft.Json.dll
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip NLog.dll
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip System.Windows.Interactivity.dll
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip Licenses\*.txt
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip Plugins\FFXIVAPP.Plugin.Event\aruba.wav
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip Plugins\FFXIVAPP.Plugin.Event\FFXIVAPP.Plugin.Event.dll
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip Plugins\FFXIVAPP.Plugin.Event\Logo.png
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip Plugins\FFXIVAPP.Plugin.Event\PluginInfo.xml
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip Plugins\FFXIVAPP.Plugin.Log\FFXIVAPP.Plugin.Log.dll
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip Plugins\FFXIVAPP.Plugin.Log\Logo.png
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip Plugins\FFXIVAPP.Plugin.Log\PluginInfo.xml
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip Plugins\FFXIVAPP.Plugin.Parse\FFXIVAPP.Plugin.Parse.dll
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip Plugins\FFXIVAPP.Plugin.Parse\Logo.png
+"C:\Program Files\7-Zip\7z.exe" a Debug.zip Plugins\FFXIVAPP.Plugin.Parse\PluginInfo.xml
+
+cd "..\Release"
+del Release.zip /q
+"C:\Program Files\7-Zip\7z.exe" a Release.zip FFXIVAPP.Client.exe
+"C:\Program Files\7-Zip\7z.exe" a Release.zip FFXIVAPP.Client.exe.config
+"C:\Program Files\7-Zip\7z.exe" a Release.zip FFXIVAPP.Client.exe.nlog
+"C:\Program Files\7-Zip\7z.exe" a Release.zip FFXIVAPP.Common.dll
+"C:\Program Files\7-Zip\7z.exe" a Release.zip FFXIVAPP.IPluginInterface.dll
+"C:\Program Files\7-Zip\7z.exe" a Release.zip FFXIVAPP.Updater.exe
+"C:\Program Files\7-Zip\7z.exe" a Release.zip HtmlAgilityPack.dll
+"C:\Program Files\7-Zip\7z.exe" a Release.zip Ionic.Zip.dll
+"C:\Program Files\7-Zip\7z.exe" a Release.zip MahApps.Metro.dll
+"C:\Program Files\7-Zip\7z.exe" a Release.zip Newtonsoft.Json.dll
+"C:\Program Files\7-Zip\7z.exe" a Release.zip NLog.dll
+"C:\Program Files\7-Zip\7z.exe" a Release.zip System.Windows.Interactivity.dll
+"C:\Program Files\7-Zip\7z.exe" a Release.zip Licenses\
+"C:\Program Files\7-Zip\7z.exe" a Release.zip Plugins\FFXIVAPP.Plugin.Event\aruba.wav
+"C:\Program Files\7-Zip\7z.exe" a Release.zip Plugins\FFXIVAPP.Plugin.Event\FFXIVAPP.Plugin.Event.dll
+"C:\Program Files\7-Zip\7z.exe" a Release.zip Plugins\FFXIVAPP.Plugin.Event\Logo.png
+"C:\Program Files\7-Zip\7z.exe" a Release.zip Plugins\FFXIVAPP.Plugin.Event\PluginInfo.xml
+"C:\Program Files\7-Zip\7z.exe" a Release.zip Plugins\FFXIVAPP.Plugin.Log\FFXIVAPP.Plugin.Log.dll
+"C:\Program Files\7-Zip\7z.exe" a Release.zip Plugins\FFXIVAPP.Plugin.Log\Logo.png
+"C:\Program Files\7-Zip\7z.exe" a Release.zip Plugins\FFXIVAPP.Plugin.Log\PluginInfo.xml
+"C:\Program Files\7-Zip\7z.exe" a Release.zip Plugins\FFXIVAPP.Plugin.Parse\FFXIVAPP.Plugin.Parse.dll
+"C:\Program Files\7-Zip\7z.exe" a Release.zip Plugins\FFXIVAPP.Plugin.Parse\Logo.png
+"C:\Program Files\7-Zip\7z.exe" a Release.zip Plugins\FFXIVAPP.Plugin.Parse\PluginInfo.xml
+
+cd "..\SmartAssembly"
+del SmartAssembly.zip /q
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip FFXIVAPP.Client.exe
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip FFXIVAPP.Client.exe.config
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip FFXIVAPP.Client.exe.nlog
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip FFXIVAPP.Common.dll
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip FFXIVAPP.IPluginInterface.dll
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip FFXIVAPP.Updater.exe
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip HtmlAgilityPack.dll
+#"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip Ionic.Zip.dll
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip MahApps.Metro.dll
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip Newtonsoft.Json.dll
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip NLog.dll
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip System.Windows.Interactivity.dll
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip Licenses\*.txt
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip Plugins\FFXIVAPP.Plugin.Event\aruba.wav
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip Plugins\FFXIVAPP.Plugin.Event\FFXIVAPP.Plugin.Event.dll
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip Plugins\FFXIVAPP.Plugin.Event\Logo.png
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip Plugins\FFXIVAPP.Plugin.Event\PluginInfo.xml
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip Plugins\FFXIVAPP.Plugin.Log\FFXIVAPP.Plugin.Log.dll
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip Plugins\FFXIVAPP.Plugin.Log\Logo.png
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip Plugins\FFXIVAPP.Plugin.Log\PluginInfo.xml
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip Plugins\FFXIVAPP.Plugin.Parse\FFXIVAPP.Plugin.Parse.dll
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip Plugins\FFXIVAPP.Plugin.Parse\Logo.png
+"C:\Program Files\7-Zip\7z.exe" a SmartAssembly.zip Plugins\FFXIVAPP.Plugin.Parse\PluginInfo.xml
