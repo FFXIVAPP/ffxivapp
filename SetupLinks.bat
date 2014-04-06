7695952e48aec80d1136fff69988271cf6ad7628
Ryan Wilson <syndicated.life@gmail.com>
2014-04-05 18:49:48 -0700

    preparing folder structure for changes in repo and moving parser into own plugin

---
+++ SetupLinks.bat
@@ -0,0 +1,6 @@
+mklink /J FFXIVAPP.Plugin.Event C:\Users\Icehunter\Documents\GitHub\ffxivapp-plugin-event\distribution
+mklink /J FFXIVAPP.Plugin.Informer C:\Users\Icehunter\Documents\GitHub\ffxivapp-plugin-informer\distribution
+mklink /J FFXIVAPP.Plugin.Log C:\Users\Icehunter\Documents\GitHub\ffxivapp-plugin-log\distribution
+mklink /J FFXIVAPP.Plugin.Parse C:\Users\Icehunter\Documents\GitHub\ffxivapp-plugin-parse\distribution
+mklink /J FFXIVAPP.Plugin.Radar C:\Users\Icehunter\Documents\GitHub\ffxivapp-plugin-radar\distribution
+mklink /J FFXIVAPP.Plugin.Widgets C:\Users\Icehunter\Documents\GitHub\ffxivapp-plugin-widgets\distribution
