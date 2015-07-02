dc9e9eeaf353adc1aa4ddb0612044100a48ef4cc
C.J. Manca <cjmanca@gmail.com>
2015-07-02 02:26:07 -0600

    Offset tweaks
    These seem to be working better for me.

--- FFXIVAPP.Client/Initializer.cs
+++ FFXIVAPP.Client/Initializer.cs
@@ -721,8 +721,10 @@ namespace FFXIVAPP.Client
                     AppViewModel.Instance.Signatures.Add(new Signature
                     {
                         Key = "TARGET",
-                        Value = "403F00000000000000000000000000000000????0000????000000000000??000000????????DB0FC940AAAA26416D30763F",
-                        Offset = 238
+                        Value = "DB0F49416F12833AFFFFFFFF00000000000000000000000000000000????????DB0FC940AAAA26416D30763FDB0FC93FDB0F49416F12833A",
+                        Offset = 0x174
+                        //Value = "403F00000000000000000000000000000000????0000????000000000000??000000????????DB0FC940AAAA26416D30763F",
+                        //Offset = 238
                     });
                     AppViewModel.Instance.Signatures.Add(new Signature
                     {
