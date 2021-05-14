8c7c5e970a9ed849cd8cf6d3dc6a7bbe8dfb6ebb
Ryan Wilson <syndicated.life@gmail.com>
2021-05-13 22:09:45 -0700

    remove distribution

--- distribution/Plugins/SetupLinks.bat
+++
@@ -1,6 +0,0 @@
-mklink /J FFXIVAPP.Plugin.Event C:\Users\Icehunter\Source\Repos\ffxivapp\ffxivapp-plugin-event\distribution
-mklink /J FFXIVAPP.Plugin.Informer C:\Users\Icehunter\Source\Repos\ffxivapp\ffxivapp-plugin-informer\distribution
-mklink /J FFXIVAPP.Plugin.Log C:\Users\Icehunter\Source\Repos\ffxivapp\ffxivapp-plugin-log\distribution
-mklink /J FFXIVAPP.Plugin.Parse C:\Users\Icehunter\Source\Repos\ffxivapp\ffxivapp-plugin-parse\distribution
-mklink /J FFXIVAPP.Plugin.Radar C:\Users\Icehunter\Source\Repos\ffxivapp\ffxivapp-plugin-radar\distribution
-mklink /J FFXIVAPP.Plugin.Widgets C:\Users\Icehunter\Source\Repos\ffxivapp\ffxivapp-plugin-widgets\distribution
